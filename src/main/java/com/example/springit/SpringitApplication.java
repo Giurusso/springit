565a2776b4b601119a50139eb0b550ccdd644da1
Giuseppe <Giurusso@gmail.com>
2020-08-13 11:59:44 +0200

    Inizializing a Database H2 from Java (part-2)

--- src/main/java/com/example/springit/SpringitApplication.java
+++ src/main/java/com/example/springit/SpringitApplication.java
@@ -1,19 +1,15 @@
 package com.example.springit;
 
-import com.example.springit.config.SpringitProperties;
 import com.example.springit.domain.Comment;
 import com.example.springit.domain.Link;
 import com.example.springit.repository.CommentRepository;
 import com.example.springit.repository.LinkRepository;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import org.slf4j.Logger;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -30,6 +26,7 @@ public class SpringitApplication {
     CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
         return args -> {
             Link link = new Link("Getting started with spring boot 2", "https://www.danvega.dev/docs/spring-boot-2-docs/#_mysql");
+
             linkRepository.save(link);
 
             Comment comment = new Comment("Fuck you and you mother !",link);
@@ -38,10 +35,9 @@ public class SpringitApplication {
 
             link.addComment(comment);
 
-
-//            System.out.println(link.toString());
             System.out.println("We just insert a link in the comment");
 
+            System.out.println("**********************************************************");
 
         };
     }
