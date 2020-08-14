3795cc015000f8149f9d373fadf7a675d6f3c122
Giuseppe <Giurusso@gmail.com>
2020-08-14 16:53:04 +0200

    Building the controller (REST)

---
+++ src/main/java/com/example/springit/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.springit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model, HttpServletRequest httpServletRequest){
+        model.addAttribute("message","Hello Rafael !!!");
+
+        return "index";
+    }
+}
