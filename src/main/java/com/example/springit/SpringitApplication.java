28d8234c542c5410da66aba34d3f5cfe4cc27221
Giuseppe <Giurusso@gmail.com>
2020-08-18 11:17:39 +0200

    View : cruds properties

--- src/main/java/com/example/springit/SpringitApplication.java
+++ src/main/java/com/example/springit/SpringitApplication.java
@@ -1,14 +1,11 @@
 package com.example.springit;
 
-import com.example.springit.domain.Comment;
-import com.example.springit.domain.Link;
-import com.example.springit.repository.CommentRepository;
-import com.example.springit.repository.LinkRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -22,22 +19,26 @@ public class SpringitApplication {
     }
 
 //    @Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
-        return args -> {
-            Link link = new Link("Getting started with spring boot 2", "https://www.danvega.dev/docs/spring-boot-2-docs/#_mysql");
-
-            linkRepository.save(link);
-
-            Comment comment = new Comment("Fuck you and you mother !",link);
-
-            commentRepository.save(comment);
-
-            link.addComment(comment);
-
-            System.out.println("We just insert a link in the comment");
-
-            System.out.println("**********************************************************");
-
-        };
-    }
+//    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
+//        return args -> {
+//            Link link = new Link("Getting started with spring boot 2", "https://www.danvega.dev/docs/spring-boot-2-docs/#_mysql");
+//
+//            linkRepository.save(link);
+//
+//            Comment comment = new Comment("Fuck you and you mother !",link);
+//
+//            commentRepository.save(comment);
+//
+//            link.addComment(comment);
+//
+//            System.out.println("We just insert a link in the comment");
+//
+//            System.out.println("**********************************************************");
+//
+//        };
+//    }
+    @Bean
+    PrettyTime prettyTime(){
+      return new PrettyTime();
+    };
 }
