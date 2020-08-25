109ba428d5ed3511234ce9a405b9b3faf14ec2c3
Giuseppe <Giurusso@gmail.com>
2020-08-25 11:40:17 +0200

    Spring security custom login form + username and password + logout + remember me

--- src/main/java/com/example/springit/security/SecurityConfiguration.java
+++ src/main/java/com/example/springit/security/SecurityConfiguration.java
@@ -21,25 +21,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-//            http.
-//                authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/link/submit").hasRole("USER")
-//                .and()
-//                .formLogin();
-
         http.authorizeRequests()
-                .requestMatchers(EndpointRequest.to("info")).permitAll()
-                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
-                .antMatchers("/actuator/").hasRole("ADMIN")
-                .antMatchers("/").permitAll()
-                .antMatchers("/link/submit").hasRole("USER")
-                .antMatchers("/h2-console/**").permitAll()
+                    .requestMatchers(EndpointRequest.to("info")).permitAll()
+                    .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                    .antMatchers("/actuator/").hasRole("ADMIN")
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/link/submit").hasRole("USER")
+                    .antMatchers("/h2-console/**").permitAll()
                 .and()
-                .formLogin()
-        .and()
-        .csrf().disable()
-        .headers().frameOptions().disable();
+                    .formLogin()
+                    .loginPage("/login").permitAll()
+                    .usernameParameter("email")
+                .and()
+                    .logout()
+                .and()
+                    .rememberMe()
+                .and()
+                    .csrf().disable()
+                    .headers().frameOptions().disable();
     }
 
     @Override
