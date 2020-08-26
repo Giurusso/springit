6eafbcf434235d9e0441e7bbf955e8387c6215b8
Giuseppe <Giurusso@gmail.com>
2020-08-26 16:38:54 +0200

    Adding comments in database and show them on page

--- src/main/java/com/example/springit/domain/Comment.java
+++ src/main/java/com/example/springit/domain/Comment.java
@@ -1,11 +1,16 @@
 package com.example.springit.domain;
 
+import com.example.springit.service.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Entity
 @RequiredArgsConstructor
@@ -23,6 +28,15 @@ public class Comment extends Auditable {
     @ManyToOne
     @NonNull
     private Link link;
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 
 
 }
